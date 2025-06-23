6c496d97e92c01fab4a14af13ba5109b9a9edcfa
HamzaDev <hmzt25235@gmail.com>
2025-06-23 05:23:15 +0300

    move main page too

--- contestmanagementsystem/app/page.tsx
+++ contestmanagementsystem/app/page.tsx
@@ -7,18 +7,7 @@ export default function Dashboard() {
       <div className="box">
         <p>Welcome to the dashboard. Choose an option below:</p>
         <div className="buttons mt-4">
-          <Link href="/contests" className="button is-primary">
-            Go to contest System
-          </Link>
-          <Link href="/sponsors" className="button is-primary">
-            Go to Sponsors System
-          </Link>
-          <Link href="/associations" className="button is-primary">
-            Go to Association System
-          </Link>
-          <Link href="/contestCategories" className="button is-primary">
-            Go to Contest Categories System
-          </Link>
+          
         </div>
       </div>
     </div>
