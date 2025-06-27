5ec876647f10215a5c39181d78abb7bd17f6afdf
HamzaDev <hmzt25235@gmail.com>
2025-06-27 20:43:24 +0300

    Adding other libraries and try to enhance structure
    Adding these libraries
    Tailwind CSS for modern UI
    ShadCN UI for modern UI
    Lucide Icons for icons
    clsx for Conditional class management
    date-fns Lightweight date formatting
    Dark mode switcher Built-in with ShadCN + Tailwind
    React-Query for fetching data
    Skelaton Loader
    Next-SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,26 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./Style/globals.css";
-import ThemeSwitcher from "@/components/ThemeSwitcher";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import "./Style/client-globals.css";
+import { Inter } from 'next/font/google';
+import { Providers } from './providers'; // react-query provider
+import { ThemeProvider } from '@/components/theme-provider'; // shadcn theme
+import SEO from '../constants/next-seo.config';
+import { NextSeo } from 'next-seo';
 
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+const inter = Inter({ subsets: ['latin'] });
+
+
+export default function RootLayout({children,}: {children: React.ReactNode;}) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <nav className="navbar">
-          <div className="navbar-end p-2">
-            <ThemeSwitcher />
-          </div>
-        </nav>
-        <main>
-          {children}
-        </main>
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <NextSeo {...SEO}/>
+      </head>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <Providers>
+            {children}
+          </Providers>
+        </ThemeProvider>
       </body>
     </html>
   );
---
+++ constants/next-seo.config.ts
@@ -0,0 +1,12 @@
+const defaultSEO = {
+    title:'FCI Hashira CLub',
+    description:'Join the most exciting contests on Hashira!',
+    openGraph:{
+        type: 'website',
+        locale: 'en_US',
+        url: 'https://hashiraclub.com/',
+        site_name: 'FCI Hashira Club',
+    }
+}
+
+export default defaultSEO;
