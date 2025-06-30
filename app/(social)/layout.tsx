df648477f9257f14e048307678fb3c14ccc0f134
HamzaDev <hmzt25235@gmail.com>
2025-06-30 19:53:26 +0300

    social media forward

---
+++ app/(social)/layout.tsx
@@ -0,0 +1,17 @@
+'use client';
+import "../Style/hashira-globals.css";
+ 
+const goToRoot = () => {
+  window.location.href = `https://linktr.ee/FCIHashira`;
+};
+
+export default function SocialLayout() {
+  goToRoot()
+  return (
+    <html>
+      <body>
+
+      </body>
+    </html>
+  );
+}
